d07bab9ecdfceb73ec0037560a35e62426f243ee
X1atle <myron.ryzleitsev@gmail.com>
2023-07-23 15:46:15 +0200

    App movement added.

--- LocPassWPF/MainWindow.xaml.cs
+++ LocPassWPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace LocPassWPF
+﻿using System.Windows.Input;
+
+namespace LocPassWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +10,28 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private bool isDragging = false;
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                isDragging = true;
+                this.DragMove();
+            }
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging && e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
         }
     }
 }
