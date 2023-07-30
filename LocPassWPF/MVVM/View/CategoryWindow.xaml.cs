1a69e4cfa5e250258ec5c7a985f3f3fcfc1f9bbf
X1atle <myron.ryzleitsev@gmail.com>
2023-07-30 22:01:18 +0200

    Global changes. Added WindowControling for optimize code. Added 2 windows(CategoryWindow and NoteWindow for buttons in main menu) Added design for new windows. Optimized MainWindow. Added additional class for mowing windows (WindowControlling).  Changed HomeView design.

---
+++ LocPassWPF/MVVM/View/CategoryWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using LocPassWPF.Core;
+
+namespace LocPassWPF.MVVM.View
+{
+    public partial class CategoryWindow : WindowControlling
+    {
+        public CategoryWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
